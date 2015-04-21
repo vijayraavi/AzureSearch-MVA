4a0c3f1629d741593fb235b2020cb5553f7ee54f
liamca <liamca@LIAMCA-X1.redmond.corp.microsoft.com>
2015-04-21 10:29:03 -0700

    Initial Checkin

---
+++ AzureSearchDotNetSample/DotNetSample/DataIndexer/Program.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Azure;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using Microsoft.Spatial;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace DataIndexer
+{
+    class Program
+    {
+        private static SearchServiceClient _searchClient;
+        private static SearchIndexClient _indexClient;
+
+        // This Sample shows how to delete, create, upload documents and query an index
+        static void Main(string[] args)
+        {
+            string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
+            string apiKey = ConfigurationManager.AppSettings["SearchServiceApiKey"];
+
+            // Create an HTTP reference to the catalog index
+            _searchClient = new SearchServiceClient(searchServiceName, new SearchCredentials(apiKey));
+            _indexClient = _searchClient.Indexes.GetClient("geonames");
+
+            Console.WriteLine("{0}", "Deleting index...\n");
+            if (DeleteIndex())
+            {
+                Console.WriteLine("{0}", "Creating index...\n");
+                CreateIndex();
+                Console.WriteLine("{0}", "Sync documents from Azure SQL...\n");
+                CreateAndSyncIndexer();
+            }
+            Console.WriteLine("{0}", "Complete.  Press any key to end application...\n");
+            Console.ReadKey();
+        }
+
+        private static bool DeleteIndex()
+        {
+            // Delete the index if it exists
+            try
+            {
+                _searchClient.Indexes.Delete("geonames");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error deleting index: {0}\r\n", ex.Message.ToString());
+                Console.WriteLine("Did you remember to add your SearchServiceName and SearchServiceApiKey to the app.config?\r\n");
+                return false;
+            }
+
+            return true;
+        }
+
+        private static void CreateIndex()
+        {
+            // Create the Azure Search index based on the included schema
+            try
+            {
+                var definition = new Index()
+                {
+                    Name = "geonames",
+                    Fields = new[] 
+                    { 
+                        new Field("FEATURE_ID",     DataType.String)         { IsKey = true,  IsSearchable = false, IsFilterable = false, IsSortable = false, IsFacetable = false, IsRetrievable = true},
+                        new Field("FEATURE_NAME",   DataType.String)         { IsKey = false, IsSearchable = true,  IsFilterable = true,  IsSortable = true,  IsFacetable = false, IsRetrievable = true},
+                        new Field("FEATURE_CLASS",  DataType.String)         { IsKey = false, IsSearchable = true,  IsFilterable = true,  IsSortable = true,  IsFacetable = false, IsRetrievable = true},
+                        new Field("STATE_ALPHA",    DataType.String)         { IsKey = false, IsSearchable = true,  IsFilterable = true,  IsSortable = true,  IsFacetable = false, IsRetrievable = true},
+                        new Field("STATE_NUMERIC",  DataType.Int32)          { IsKey = false, IsSearchable = false, IsFilterable = true,  IsSortable = true,  IsFacetable = true,  IsRetrievable = true},
+                        new Field("COUNTY_NAME",    DataType.String)         { IsKey = false, IsSearchable = true,  IsFilterable = true,  IsSortable = true,  IsFacetable = false, IsRetrievable = true},
+                        new Field("COUNTY_NUMERIC", DataType.Int32)          { IsKey = false, IsSearchable = false, IsFilterable = true,  IsSortable = true,  IsFacetable = true,  IsRetrievable = true},
+                        new Field("ELEV_IN_M",      DataType.Int32)          { IsKey = false, IsSearchable = false, IsFilterable = true,  IsSortable = true,  IsFacetable = true,  IsRetrievable = true},
+                        new Field("ELEV_IN_FT",     DataType.Int32)          { IsKey = false, IsSearchable = false, IsFilterable = true,  IsSortable = true,  IsFacetable = true,  IsRetrievable = true},
+                        new Field("MAP_NAME",       DataType.String)         { IsKey = false, IsSearchable = true,  IsFilterable = true,  IsSortable = true,  IsFacetable = false, IsRetrievable = true},
+                        new Field("DESCRIPTION",    DataType.String)         { IsKey = false, IsSearchable = true,  IsFilterable = false, IsSortable = false, IsFacetable = false, IsRetrievable = true},
+                        new Field("HISTORY",        DataType.String)         { IsKey = false, IsSearchable = true,  IsFilterable = false, IsSortable = false, IsFacetable = false, IsRetrievable = true},
+                        new Field("LOCATION",       DataType.GeographyPoint) { IsKey = false, IsSearchable = false, IsFilterable = true,  IsSortable = false, IsFacetable = false, IsRetrievable = true},
+                        new Field("DATE_CREATED",   DataType.DateTimeOffset) { IsKey = false, IsSearchable = false, IsFilterable = true,  IsSortable = true,  IsFacetable = true,  IsRetrievable = true},
+                        new Field("DATE_EDITED",    DataType.DateTimeOffset) { IsKey = false, IsSearchable = false, IsFilterable = true,  IsSortable = true,  IsFacetable = true,  IsRetrievable = true}
+                    }
+                };
+
+                _searchClient.Indexes.Create(definition);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error creating index: {0}\r\n", ex.Message.ToString());
+            }
+
+        }
+
+        private static void CreateAndSyncIndexer()
+        {
+            // Create a new indexer and sync it
+            try
+            {
+                DataSource ds = new DataSource();
+                ds.Name = "usgs-datasource";
+                ds.Description = "USGS Dataset";
+                ds.Type = "azuresql";
+                ds.Credentials = new DataSourceCredentials("Server=tcp:azs-playground.database.windows.net,1433;Database=usgs;User ID=reader;Password=EdrERBt3j6mZDP;Trusted_Connection=False;Encrypt=True;Connection Timeout=30");
+                ds.Container = new DataContainer("GeoNamesRI");
+
+                Indexer idx = new Indexer();
+                idx.Name = "usgs-indexer";
+                idx.Description = "USGS data indexer";
+                idx.DataSourceName = "usgs-datasource";
+                idx.TargetIndexName = "geonames";
+                idx.Parameters = new IndexingParameters();
+                idx.Parameters.MaxFailedItems = 10;
+                idx.Parameters.MaxFailedItemsPerBatch = 5;
+                idx.Parameters.Base64EncodeKeys = false;
+
+                //Delete indexer and datasource if it existed
+                _searchClient.DataSources.Delete("usgs-datasource");
+                _searchClient.Indexers.Delete("usgs-indexer");
+
+                //Create indexer and datasource
+                _searchClient.DataSources.Create(ds);
+                _searchClient.Indexers.Create(idx);
+
+                //Launch the sync and then monitor progress until complete
+                AzureOperationResponse response = _searchClient.Indexers.Run("usgs-indexer");
+                IndexerGetStatusResponse statusResponse;
+                do
+                {
+                    // Check status every 3 seconds
+                    Thread.Sleep(3000);
+                    statusResponse = _searchClient.Indexers.GetStatus("usgs-indexer");
+                    Console.WriteLine("Sync {0}...", statusResponse.ExecutionInfo.LastResult.Status.ToString());
+                } while (statusResponse.ExecutionInfo.LastResult.Status.ToString() == "InProgress");
+                Console.WriteLine("Synchronized {0} rows.\n", statusResponse.ExecutionInfo.LastResult.ItemCount.ToString());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error creating indexer: {0}: \n", ex.Message.ToString());
+            }
+
+        }
+    }
+}
